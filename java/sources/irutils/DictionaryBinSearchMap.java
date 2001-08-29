5b4bad98c2cf0d79bc453cab654a33f9fc7410ee
wrogers <wrogers>
2001-08-29 02:28:31 +0000

    Prise style dictionary classes

---
+++ java/sources/irutils/DictionaryBinSearchMap.java
@@ -0,0 +1,116 @@
+package utils;
+import java.io.*;
+
+/**
+ * DictionaryBinSearchMap.java
+ *
+ *
+ * Created: Wed Jul 25 09:09:18 2001
+ *
+ * @author <a href="mailto:wrogers@nlm.nih.gov">Willie Rogers</a>
+ * @version $Id: DictionaryBinSearchMap.java,v 1.1 2001/08/29 02:28:31 wrogers Exp $
+ */
+
+public class DictionaryBinSearchMap implements BinSearchMap, Serializable {
+  // Organization of dictionary one record:
+  //  +------------------------+--------------------+-------------------+
+  //  | term                   | number of postings |     address       |
+  //  +------------------------+--------------------+-------------------+
+  //  |<---- term length ----->|<---- 4 bytes ----->|<---- 4 bytes ---->|
+  //  |<-------------------------- record length ---------------------->|
+  //
+  //  Term Length, # of postings And addr are the same for all records in a partition.
+
+  /** length of integer in bytes */
+  public static final int DATALENGTH = 4; /* is this right */
+  /** data output stream for writing map. */
+  private transient DataOutputStream mapWriter;
+  /** random access file for reading map. */
+  private transient RandomAccessFile mapRAFile;
+  /** number of records in this map. */
+  int numberOfRecords = 0;
+  /** term length of all terms in this map. */
+  private int termLength = 0;
+  /** filename of map */
+  String filename;
+
+  /**
+   * Instantiate a new or existing binary search map with single integers for data.
+   * @param mapFilename filename of map
+   * @param mode        file mode to use: utils.DictionaryBinSearchMap.WRITE to open map for writing, and
+   *                    utils.DictionaryBinSearchMap.READ to open map for reading.
+   */
+  public DictionaryBinSearchMap ( String mapFilename, int mode )
+    throws FileNotFoundException
+  {
+    if ( mode == WRITE ) {
+      this.mapWriter = 
+	new DataOutputStream ( new BufferedOutputStream
+			       ( new FileOutputStream ( mapFilename )));
+    } else {
+      this.mapRAFile = new RandomAccessFile ( mapFilename, "r");
+    }
+    this.filename = mapFilename;
+  }
+
+  /**
+   * Write an entry into map
+   * @param term Term. 
+   * @param numberOfPostings Number of postings assoicated with term.
+   * @param address Address of postings assoicated with term.
+   */
+  public void writeEntry(String term, int numOfPostings, int data)
+    throws IOException
+  {
+    // write dictionary entry
+    this.mapWriter.writeChars(term);
+    this.mapWriter.writeInt(numOfPostings);
+    this.mapWriter.writeInt(data);
+    this.numberOfRecords++;
+    this.termLength = term.length();
+  }
+
+  /**
+   * get data entry for term 
+   * @param term term 
+   * @return dictionary entry associated with term
+   */
+  public DictionaryEntry get(String term)
+    throws IOException
+  {
+    if (this.mapRAFile == null ) {
+       this.mapRAFile = new RandomAccessFile ( this.filename, "r");
+    }
+    return DiskBinarySearch.dictionaryBinarySearch(this.mapRAFile, 
+					    term, term.length(), this.numberOfRecords);
+  }
+
+  /** 
+   * @return get number of records in map
+   */
+  public int getNumberOfRecords()
+  {
+    return this.numberOfRecords;
+  }
+
+  /**
+   * @return get length of data in each record
+   */
+  public int getDataLength()
+  {
+    return this.DATALENGTH;
+  }
+
+  /** close resources used by this map. */
+  public void close()
+    throws IOException
+  {
+    if (this.mapRAFile != null ) {
+      this.mapRAFile.close();
+    }
+    if (this.mapWriter != null ) {
+      this.mapWriter.close();
+    }
+  }
+
+}// DictionaryBinSearchMap
---
+++ java/sources/irutils/DictionaryEntry.java
@@ -0,0 +1,52 @@
+package utils;
+
+ /**
+ * DictionaryEntry.java
+ *
+ *
+ * Created: Mon Aug 27 13:07:37 2001
+ *
+ * @author <a href="mailto:wrogers@nlm.nih.gov">Willie Rogers</a>
+ * @version $Id: DictionaryEntry.java,v 1.1 2001/08/29 02:28:31 wrogers Exp $
+ */
+
+public class DictionaryEntry
+{
+
+  // Organization of dictionary one record:
+  //  +------------------------+--------------------+-------------------+
+  //  | term                   | number of postings |     address       |
+  //  +------------------------+--------------------+-------------------+
+  //  |<---- term length ----->|<---- 4 bytes ----->|<---- 4 bytes ---->|
+  //  |<-------------------------- record length ---------------------->|
+  //
+  //  Term Length, # of postings And addr are the same for all records in a partition.
+
+  String term;
+  int numberOfPostings;
+  int address;
+  
+  public DictionaryEntry (String term, int numberOfPostings, int address)
+  {
+    this.term = term;
+    this.numberOfPostings = numberOfPostings;
+    this.address = address;
+  }
+  
+  public String getTerm()
+  {
+    return this.term;
+  }
+
+  public int getNumberOfPostings()
+  {
+    return this.numberOfPostings;
+  }
+  public int getAddress()
+  {
+    return this.address;
+  }
+}// DictionaryEntry
+
+
+
