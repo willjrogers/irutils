ae0321c53ed332bf69dca05f4b6800d62f3c007b
wrogers <wrogers>
2002-03-19 15:09:04 +0000

    JDE project file

---
+++ java/prj.el
@@ -0,0 +1,5 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-global-classpath (quote ("./classes")))
+ '(jde-compile-option-directory "./classes")
+ '(jde-compile-option-debug (quote ("all" (t t t)))))
